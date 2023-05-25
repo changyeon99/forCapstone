24c980843453b9a25f1979df1b50bf3f76dab243
LimJangCheon <jangchone123@gmail.com>
2023-05-25 17:56:46 +0900

    23.05.25

--- FLAME.py
+++ FLAME.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-import OS
+import os
 
 channel = 17  # BCM. 17, wPi. 0, Physical. 11(DOUT에 연결)
 
--- pushbullet.sh
+++ pushbullet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 API="o.vaPPxpIea8K6JRg7yHk1rap8OV1jPISU"
-MSG="$1"
 
-curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="WARNING" -d body="$MSG"
+curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="불이야" -d body="화재 경보"
