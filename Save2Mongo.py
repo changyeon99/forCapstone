0f41fce240f5d2ef0bd255dc56bf5f728ef1855a
PAK <pjk980917@naver.com>
2023-05-28 00:05:34 +0900

    23.05.28

--- Save2Mongo.py
+++ Save2Mongo.py
@@ -33,20 +33,21 @@ data_list = []  # 데이터를 저장할 리스트
 try:
     while True:
         now = datetime.now()    
-        if now.second == 0:  # 현재 시각의 초가 0초인 경우에만 데이터 저장
+        if now.second == 0:  # 현재 시각의 초가 0초인 경우에만 데이터 저장 
             file_name = time.strftime('%Y-%m-%d %H:%M') + '.csv' # csv파일명 올바르게 수정해야함.
+            coll_name = time.strftime('%Y-%m-%d %H:%M')
             while True:
                 start_time = datetime.now()
                 if start_time.second != 0:
                     df = pd.DataFrame([line.strip().split(',') for line in data_list], columns=['시각','음향2','적외선2']) # 컬럼명을 올바르게 수정해야함.
                     df.to_csv(file_name) # csv파일명 올바르게 수정해야함.
-                    collection = db[file_name] # '세부 데이터베이스 이름' --> file_name으로 변경하면 됨 / file_name = time.strftime('%Y-%m-%d %H:%M') + '.txt'
+                    collection = db[coll_name] # '세부 데이터베이스 이름' --> file_name으로 변경하면 됨 / file_name = time.strftime('%Y-%m-%d %H:%M') + '.txt'
                     df = pd.read_csv(file_name, encoding='cp949') # file_name
                     # df = pd.read_table("파일 위치 및 파일명", sep=',', encoding='cp949') # 해당 코드는 CSV파일이 아닌 txt파일인 경우 데이터프레임으로 변환시켜주는 코드 입니다.
                     df_dict = df.to_dict(orient= 'records')
                     collection.insert_many(df_dict) # 딕셔너리 형태의 데이터를 세부 DB에 추가한다.
                     # 딕셔너리 형태의 데이터를 추가할 시 매번 id도 추가 되기 때문에 id를 제거하기 위한 코드입니다.
-                    pd.DataFrame(list(collection.find())).drop('_id', axis=1) 
+                    # print(pd.DataFrame(list(collection.find())).drop(['_id','Unnamed: 0'], axis=1))
                     os.remove(file_name) # 임시로 생성한 CSV 파일을 삭제합니다.
                     now = datetime.now()
                     time_difference = now.replace(second=59) - now
