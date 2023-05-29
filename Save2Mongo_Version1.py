1e1a39d099c541548cbc1df93e19043ce1599af3
PAK <pjk980917@naver.com>
2023-05-29 20:16:04 +0900

    23.05.29

--- Save2Mongo_Version1.py
+++ Save2Mongo_Version1.py
@@ -31,43 +31,50 @@ from datetime import datetime
 # 시리얼 포트 설정
 ser = serial.Serial('/dev/ttyACM0', 2000000) # 포트 번호와 통신속도를 아두이노를 동일하게 설정해야함.
 
-NUM = 0 # collection 값 500 이상시 데이터 저장이 중지(499까지는 저장됨) 되기 때문에 생성한 메인 DB 생성 초기화를 위한 변수
+NUM = 1 # collection 값 500 이상시 데이터 저장이 중지(499까지는 저장됨) 되기 때문에 생성한 메인 DB 생성 초기화를 위한 변수
 
 # 데이터 읽기 및 저장
 try:
     while True:
         now = datetime.now()
         if now.second == 0:  # 현재 시각의 초가 0초인 경우에만 데이터 저장
-            for i in range(481):
-                NUM += 1
-                if NUM == 481:
-                    hour = 1
-                    # 새로운 메인 DB 생성하기
-                    db = client['CMPS_TimingBelt_Data_After_' + 8*hour] # db 컨택 / 없는것도 바로 생성 가능
-                    NUM = 0
-                    hour += 1
-                file_name = time.strftime('%Y-%m-%d %H:%M') + '.csv' # csv파일명 올바르게 수정해야함.
-                coll_name = time.strftime('%Y-%m-%d %H:%M')
-                data_list = []  # 데이터를 저장할 리스트
-                while True:
-                    start_time = datetime.now()
-                    if start_time.second != 0:
-                        df = pd.DataFrame([line.strip().split(',') for line in data_list]) # 컬럼명을 올바르게 수정해야함.
-                        df.to_csv(file_name) # csv파일명 올바르게 수정해야함.
-                        collection = db[coll_name] # '세부 데이터베이스 이름' --> file_name으로 변경하면 됨 / file_name = time.strftime('%Y-%m-%d %H:%M') + '.txt'
-                        df = pd.read_csv(file_name, encoding='cp949') # file_name
-                        # df = pd.read_table("파일 위치 및 파일명", sep=',', encoding='cp949') # 해당 코드는 CSV파일이 아닌 txt파일인 경우 데이터프레임으로 변환시켜주는 코드 입니다.
-                        df_dict = df.to_dict(orient= 'records')
-                        collection.insert_many(df_dict) # 딕셔너리 형태의 데이터를 세부 DB에 추가한다.
-                        # 딕셔너리 형태의 데이터를 추가할 시 매번 id도 추가 되기 때문에 id를 제거하기 위한 코드입니다.
-                        # print(pd.DataFrame(list(collection.find())).drop(['_id','Unnamed: 0'], axis=1))
-                        os.remove(file_name) # 임시로 생성한 CSV 파일을 삭제합니다.
-                        now = datetime.now()
-                        time_difference = now.replace(second=59) - now
-                        time.sleep(time_difference.total_seconds())
-                        break ## 일시정지 후 While문 탈출.
-                    data = ser.readline().decode().strip() # 시리얼 포트로부터 데이터 읽기
-                    data_list.append(str(start_time.date()) + str(start_time.time()) + ',' + data)  # 데이터를 리스트에 추가
+            file_name = time.strftime('%Y-%m-%d %H:%M') + '.csv' # csv파일명 올바르게 수정해야함.
+            coll_name = time.strftime('%Y-%m-%d %H:%M')
+            data_list = []  # 데이터를 저장할 리스트
+            while True:
+                start_time = datetime.now()
+                if start_time.second != 0:
+                    df = pd.DataFrame([line.strip().split(',') for line in data_list]) # 컬럼명을 올바르게 수정해야함.
+                    df.to_csv(file_name) # csv파일명 올바르게 수정해야함.
+                    collection = db[coll_name] # '세부 데이터베이스 이름' --> file_name으로 변경하면 됨 / file_name = time.strftime('%Y-%m-%d %H:%M') + '.txt'
+                    df = pd.read_csv(file_name, encoding='cp949') # file_name
+                    # df = pd.read_table("파일 위치 및 파일명", sep=',', encoding='cp949') # 해당 코드는 CSV파일이 아닌 txt파일인 경우 데이터프레임으로 변환시켜주는 코드 입니다.
+                    df_dict = df.to_dict(orient= 'records')
+                    collection.insert_many(df_dict) # 딕셔너리 형태의 데이터를 세부 DB에 추가한다.
+                    # 딕셔너리 형태의 데이터를 추가할 시 매번 id도 추가 되기 때문에 id를 제거하기 위한 코드입니다.
+                    # print(pd.DataFrame(list(collection.find())).drop(['_id','Unnamed: 0'], axis=1))
+                    os.remove(file_name) # 임시로 생성한 CSV 파일을 삭제합니다.
+                    now = datetime.now()
+                    time_difference = now.replace(second=59) - now
+                    time.sleep(time_difference.total_seconds())
+                    if NUM == 480:
+                        hour = 1
+                        # 새로운 메인 DB 생성하기
+                        # CMPS : 측면파손
+                        # CHBJ : 치형빠짐
+                        # SSJD : 심선절단
+                        # SSNC : 심선노출
+                        # GMGY : 고무균열
+                        db = client['CMPS_TimingBelt_Data_After_' + 8*hour] # db 컨택 / 없는것도 바로 생성 가능
+                        hour += 1
+                        NUM = 1
+                        break
+                    else:
+                        NUM += 1
+                        continue
+                    break ## 일시정지 후 While문 탈출.
+                data = ser.readline().decode().strip() # 시리얼 포트로부터 데이터 읽기
+                data_list.append(str(start_time.date()) + str(start_time.time()) + ',' + data)  # 데이터를 리스트에 추가
 
 
 except KeyboardInterrupt:
